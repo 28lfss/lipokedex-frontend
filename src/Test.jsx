33f6aa2f50ba58cc33dec729b5a102c3676e2c76
lfssa <28lfss@gmail.com>
2025-06-21 21:33:19 -0300

    Added logo and icon images

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import SearchBar from './components/searchbar/SearchBar'
+import logo from './assets/logo.png'
 
 function Test() {
     return (
         <>
+            <img src={logo} alt="logo" />
+
             <SearchBar />
         </>
     )
